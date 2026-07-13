40495313f2ca7ab87e4fe2a1e1266f722f9f2419
Anjaligupta63 <150386483+Anjaligupta63@users.noreply.github.com>
2026-07-13 18:19:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
